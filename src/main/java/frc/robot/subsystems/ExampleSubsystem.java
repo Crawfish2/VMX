d90bb68b11fed166a917565f553708d69854b329
Crawfish2 <umakawata@gmail.com>
2025-03-21 10:35:21 +0900

    srcをサンプルのものに置き換え

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,18 +10,39 @@ import java.util.Map;
 
 import frc.robot.Constants;
 import com.studica.frc.TitanQuad;
-import com.studica.frc.TitanQuadEncode;
-import frc.robot.
+import com.studica.frc.TitanQuadEncoder;
+
+
 public class ExampleSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
-  public ExampleSubsystem() {
+  
+  private TitanQuad motor0;
+  private TitanQuadEncoder motor0_enc;
+  
+  private ShuffleboardTab tab = Shuffleboard.getTab("VMX");
+  
+  private NetworkTableEntry motorPos0 = tab.add("Motor0", 0)
+                                          .withWidget(BuiltInWidgets.kNumberSlider)
+                                          .withProperties(Map.of("min", -1, "max", 1))
+                                          .getEntry(); 
 
-  }
-
+  private NetworkTableEntry M0EncoderValue = tab.add("M0", 0) .getEntry();                                        
+  private NetworkTableEntry M0LimH_Value = tab.add("M0 nLim.H", 0) .getEntry();              
+  private NetworkTableEntry M0LimL_Value = tab.add("M0 nLim.L", 0) .getEntry();              
+  public ExampleSubsystem()
+    {
+    motor0 = new TitanQuad(Constants.TITAN_ID, Constants.MOTOR0);
+    motor0_enc = new TitanQuadEncoder(motor0, Constants.MOTOR0, Constants.WHEEL_DIST_PER_TICK);
+    motorPos0.setDouble(0);
+   
+    motor0_enc.reset();
+    }
+  
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic()
+    {
+     motor0.set(motorPos0.getDouble(0.0));
+     M0EncoderValue.setDouble( motor0_enc.getEncoderDistance());
+     M0LimH_Value.setDouble( motor0.getLimitSwitch(0,false)); //direction nLim.H = false
+     M0LimL_Value.setDouble( motor0.getLimitSwitch(0,true)); //direction nLim.L = true
+    }
 }
