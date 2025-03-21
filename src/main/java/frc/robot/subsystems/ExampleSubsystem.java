83406cd8eed01cb86e307a4ef62fb881316854f0
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-03-21 11:34:15 +0900

    スライダーでロボットの移動する速度と向きを変えられるようにする

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import frc.robot.Constants;
@@ -16,45 +17,64 @@ public class ExampleSubsystem extends SubsystemBase {
 
   private TitanQuad[] motors;
   private TitanQuadEncoder[] motors_enc;
-  private NetworkTableEntry[] motorsPos;
   private NetworkTableEntry[] MotorsEncoderValue;
   private NetworkTableEntry[] MotorsLimH_Value;
   private NetworkTableEntry[] MotorsLimL_Value;
+
+  private NetworkTableEntry globalSpeed;
+  private NetworkTableEntry globalAngle;
 
   private ShuffleboardTab tab = Shuffleboard.getTab("VMX");
 
   public ExampleSubsystem() {
     motors = new TitanQuad[4];
     motors_enc = new TitanQuadEncoder[4];
-    motorsPos = new NetworkTableEntry[4];
     MotorsEncoderValue = new NetworkTableEntry[4];
     MotorsLimH_Value = new NetworkTableEntry[4];
     MotorsLimL_Value = new NetworkTableEntry[4];
 
-    
+    globalSpeed = tab.add("Global Speed", 0.0)
+        .withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min", -1.0, "max", 1.0))
+        .getEntry();
+
+    globalAngle = tab.add("Global Angle", 0.0)
+        .withWidget(BuiltInWidgets.kNumberSlider)
+        .withProperties(Map.of("min", -180.0, "max", 180.0))
+        .getEntry();
 
     for (int i = 0; i < 4; i++) {
       motors[i] = new TitanQuad(Constants.TITAN_ID, i);
       motors_enc[i] = new TitanQuadEncoder(motors[i], i, Constants.WHEEL_DIST_PER_TICK);
 
-      motorsPos[i] = tab.add("Motor" + i, 0)
-          .withWidget(BuiltInWidgets.kNumberSlider)
-          .withProperties(Map.of("min", -1, "max", 1))
-          .getEntry();
-
       MotorsEncoderValue[i] = tab.add("M" + i, 0).getEntry();
       MotorsLimH_Value[i] = tab.add("M" + i + " nLim.H", 0).getEntry();
       MotorsLimL_Value[i] = tab.add("M" + i + " nLim.L", 0).getEntry();
 
-      motorsPos[i].setDouble(0);
       motors_enc[i].reset();
     }
   }
 
   @Override
   public void periodic() {
+    double speed = globalSpeed.getDouble(0.0);
+    double angle = globalAngle.getDouble(0.0);
+
+    double[] motorsSpeed = new double[3];
+    motorsSpeed[0] = speed * Math.sin(Math.toRadians(angle - 60));
+    motorsSpeed[1] = speed * Math.sin(Math.toRadians(angle + 60));
+    motorsSpeed[2] = speed * Math.sin(Math.toRadians(angle - 180));
+
+    double x = Arrays.stream(motorsSpeed).map(s -> Math.abs(s)).max().getAsDouble();
+
+    motorsSpeed[0] = motorsSpeed[0] * speed / x;
+    motorsSpeed[1] = motorsSpeed[1] * speed / x;
+    motorsSpeed[2] = motorsSpeed[2] * speed / x;
+
+    motors[0].set(motorsSpeed[0]);
+    motors[1].set(motorsSpeed[1]);
+    motors[2].set(motorsSpeed[2]);
     for (int i = 0; i < 4; i++) {
-      motors[i].set(motorsPos[i].getDouble(0.0));
       MotorsEncoderValue[i].setDouble(motors_enc[i].getEncoderDistance());
       MotorsLimH_Value[i].setDouble(motors[i].getLimitSwitch(i, false));
       MotorsLimL_Value[i].setDouble(motors[i].getLimitSwitch(i, true));
