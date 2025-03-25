af50dd9e284735262a942ee98ba2c7e427ef94e3
Crawfish2 <umakawata@gmail.com>
2025-03-25 13:36:16 +0900

    テストプログラム書き込み

--- src/main/java/frc/robot/commands/test1/DriveTri.java
+++ src/main/java/frc/robot/commands/test1/DriveTri.java
@@ -7,12 +7,10 @@ import frc.robot.commands.driveCommands.Rotate;
 public class DriveTri extends AutoCommand {
     public DriveTri() {
         super(new Drive(90).withTimeout(2.5),
+                new Rotate(0.3).withTimeout(3),
                 new Drive(0).withTimeout(10),
-                new Drive(-90).withTimeout(5),
-                new Rotate(0.3).withTimeout(5),
-                new Drive(0).withTimeout(10),
-                new Rotate(0.3).withTimeout(2.5),
-                new Drive(0).withTimeout(2.5)
+                new Rotate(-0.3).withTimeout(3),
+                new Drive(0).withTimeout(10)
         );
     }
 }
