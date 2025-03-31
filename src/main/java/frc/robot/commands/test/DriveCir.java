34d68cc252d459357bfc9473db69540b58c834d5
Crawfish2 <umakawata@gmail.com>
2025-03-31 09:36:26 +0900

    円形に動くコマンド仮

--- src/main/java/frc/robot/commands/test/DriveCir.java
+++ src/main/java/frc/robot/commands/test/DriveCir.java
@@ -4,10 +4,15 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.auto.AutoCommand;
 import frc.robot.commands.driveCommands.Drive;
 import frc.robot.commands.driveCommands.Rotate;
+import frc.robot.commands.driveCommands.Stop;;
 
-
-public class DriveCir extends ParallelCommandGroup {
-    
-
-
+public class DriveCir extends AutoCommand {
+    public DriveCir() {
+        super(
+                new Rotate(0.3).withTimeout(1.5),
+                new ParallelCommandGroup(
+                        new Drive(0).withTimeout(2),
+                        new Rotate(-0.3).withTimeout(2)),
+                new Stop().withTimeout(0.1));
+    }
 }
