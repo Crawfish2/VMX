be445e9feb5fac3293f287f9c7ccb0dbd68fabf6
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-04-20 19:22:05 +0900

    DriveCirが例外を投げる問題を修正

--- src/main/java/frc/robot/commands/test/DriveCir.java
+++ src/main/java/frc/robot/commands/test/DriveCir.java
@@ -1,14 +1,11 @@
 package frc.robot.commands.test;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.auto.AutoCommand;
 import frc.robot.subsystems.TitanKilloughDrive;;
 
 public class DriveCir extends AutoCommand {
   public DriveCir(TitanKilloughDrive drive) {
-    super(
-        new ParallelCommandGroup(
-            drive.DriveCommand(90).withTimeout(2),
-            drive.RotateCommand(-0.3).withTimeout(2)));
+    // 前進しつつ、時計回りに回転する
+    super(drive.DrivePolarCommand(0.3, 90, 0.3).withTimeout(2));
   }
 }
