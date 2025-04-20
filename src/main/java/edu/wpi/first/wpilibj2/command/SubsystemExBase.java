a481641b1fd73993abb42ad3f323ee7a8b38b7b9
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-04-20 13:25:45 +0900

    format

--- src/main/java/edu/wpi/first/wpilibj2/command/SubsystemExBase.java
+++ src/main/java/edu/wpi/first/wpilibj2/command/SubsystemExBase.java
@@ -4,5 +4,3 @@ package edu.wpi.first.wpilibj2.command;
 /** コマンド作成メソッドを追加したSubSystemBase */
 public abstract class SubsystemExBase extends SubsystemBase implements SubSystemEx {
 }
-
-
--- src/main/java/frc/robot/subsystems/SimpleCamera.java
+++ src/main/java/frc/robot/subsystems/SimpleCamera.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -14,11 +17,24 @@ public class SimpleCamera extends SubsystemBase {
   private final int height = 120;
   private final int fps = 5;
 
+  /** String Publisher */
+  private final NetworkTableEntry requestEntry;
+
+  /** String Subscriber */
+  private final NetworkTableEntry resultEntry;
+
   public SimpleCamera() {
     camera = cameraServer.startAutomaticCapture();
     camera.setVideoMode(VideoMode.PixelFormat.kMJPEG, width, height, fps);
+
+    final var nt = NetworkTableInstance.getDefault();
+    requestEntry = nt.getEntry("/ColorSquares/Request");
+    resultEntry = nt.getEntry("/ColorSquares/Result");
   }
 
-  @Override
-  public void periodic() {}
+  public void postRequest() {
+    requestEntry.setString("Detect");
+    resultEntry.addListener(notification -> {
+    }, EntryListenerFlags.kUpdate);
+  }
 }
