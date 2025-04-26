a115293bf862a5479af45bd86d924520dedad894
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-04-26 16:41:43 +0900

    TitanQuadPIDの調整

--- src/main/java/frc/robot/util/TitanQuadPID.java
+++ src/main/java/frc/robot/util/TitanQuadPID.java
@@ -1,30 +1,46 @@
 package frc.robot.util;
 
 import com.studica.frc.TitanQuad;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 
 
-public class TitanQuadPID extends TitanQuad {
+public class TitanQuadPID extends TitanQuad implements Sendable {
   private final PIDController controller;
 
   private final double kP = 0.04;
-  private final double kD = 0.004;
+  private final double kD = 0.002;
 
   public TitanQuadPID(int deviceID, int frequency, int motor) {
     super(deviceID, frequency, motor);
     controller = new PIDController(kP, 0.0, kD);
+    SendableRegistry.addChild(this, controller);
   }
 
   public TitanQuadPID(int deviceID, int motor) {
     super(deviceID, motor);
     controller = new PIDController(kP, 0.0, kD);
+    SendableRegistry.addChild(this, controller);
   }
 
   @Override
   public void set(double speed) {
-    final var rpm = getRPM();
-    final var target = speed * 80;
+    final var maxRPM = 120;
+    final var rpm = (double) -getRPM() / maxRPM;
+    final var target = speed;
     final var fix = controller.calculate(rpm, target);
-    super.set(speed + fix);
+    final var setSpeed = speed + fix;
+    super.set(setSpeed);
   }
+
+  @Override
+  public void stopMotor() {
+    super.stopMotor();
+    controller.calculate(0, 0);
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {}
 }
