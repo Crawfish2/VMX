0806c32de3562ad86ddff7fb54c3ff51393f084a
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-07-06 12:42:58 +0900

    SaferMotorをElevator.javaからファイル分割する

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,15 +2,13 @@ package frc.robot.subsystems;
 
 import com.studica.frc.TitanQuad;
 import com.studica.frc.TitanQuadEncoder;
-import edu.wpi.first.wpilibj.MotorSafety;
-import edu.wpi.first.wpilibj.Sendable;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemExBase;
+import frc.robot.util.SaferMotor;
 import static frc.robot.Constants.TitanConstants.TITAN_ID;
 import static frc.robot.Constants.TitanConstants.ElevatorConstants.ELEVATOR;
 import static frc.robot.Constants.TitanConstants.ElevatorConstants.WHEEL_DIST_PER_TICK;
@@ -110,51 +108,3 @@ public class Elevator extends SubsystemExBase {
   }
 }
 
-
-class SaferMotor extends MotorSafety implements Sendable, AutoCloseable {
-  private final SpeedController motor;
-
-  public SaferMotor(SpeedController motor) {
-    this.motor = motor;
-
-    // 一定時間経過後に、自動で停止する
-    setSafetyEnabled(true);
-
-    registerToShuffleboard();
-  }
-
-  /** Shuffleboardへの登録をする */
-  private void registerToShuffleboard() {
-    SendableRegistry.addChild(this, this.motor);
-    SendableRegistry.addLW(this, "SaferMotor");
-  }
-
-  public void drive(double speed) {
-    motor.set(speed);
-    feed();
-  }
-
-  @Override
-  public void close() {
-    SendableRegistry.remove(this);
-  }
-
-  @Override
-  public String getDescription() {
-    return "SaferMotor";
-  }
-
-  @Override
-  public void stopMotor() {
-    motor.stopMotor();
-    feed();
-  }
-
-  @Override
-  public void initSendable(SendableBuilder builder) {
-    builder.setSmartDashboardType("SaferMotor");
-    builder.setActuator(true);
-    builder.setSafeState(this::stopMotor);
-    builder.addDoubleProperty("Motor Speed", motor::get, this::drive);
-  }
-}
