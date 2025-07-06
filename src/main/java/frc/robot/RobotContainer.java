89645c57fb2069b691d38e85b1aecbca30ec7778
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-07-06 12:48:03 +0900

    コントローラーのボタン割り当て処理を、configureButtonBindings()に移動する

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -46,8 +46,15 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+  }
 
-
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
     // 左スティック X,Y: 水平移動
     // 右スティック X: 回転
     // R1, R2: エレベーター下降、上昇
@@ -80,15 +87,6 @@ public class RobotContainer {
     // }
     // }, arm));
   }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
-
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
