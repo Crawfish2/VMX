35a1d054386712a421a21050402b27fb9e121cd6
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2025-07-23 09:21:59 +0900

    使用していないコマンドを削除する

--- src/main/java/frc/robot/util/CommandTester.java
+++ src/main/java/frc/robot/util/CommandTester.java
@@ -10,32 +10,21 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.task.DriveCorners;
-import frc.robot.commands.test.DriveTri;
 import frc.robot.subsystems.NewElevator;
-import frc.robot.subsystems.SimpleCamera;
 import frc.robot.subsystems.TitanKilloughDrive;
-import frc.robot.subsystems.UltraSonicSensor;
 
 public class CommandTester {
   private final SendableChooser<Supplier<Command>> chooser;
 
   private final NetworkTableEntry speed;
-  private final NetworkTableEntry angle;
   private final NetworkTableEntry distance;
 
-  public CommandTester(TitanKilloughDrive drive, UltraSonicSensor sonar, SimpleCamera camera,
-      NewElevator elevator) {
+  public CommandTester(TitanKilloughDrive drive, NewElevator elevator) {
     final ShuffleboardTab tab = Shuffleboard.getTab("CommandTester");
 
     speed = tab.add("Speed", 0.0)
         .withWidget(BuiltInWidgets.kNumberSlider)
         .withProperties(Map.of("min", -1.0, "max", 1.0))
-        .getEntry();
-
-    angle = tab.add("Angle", 0.0)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", -360.0, "max", 360.0))
         .getEntry();
 
     distance = tab.add("Distance", 0.0)
@@ -48,15 +37,6 @@ public class CommandTester {
     chooser.setDefaultOption("DriveMotor",
         () -> new SequentialCommandGroup(drive.DriveCommand(speed.getDouble(0.0)).withTimeout(5)));
 
-    chooser.addOption("Rotate",
-        () -> drive.RotateDistanceCommand(angle.getDouble(0.0)).withTimeout(5));
-    chooser.addOption("DriveCorners", () -> new DriveCorners(drive));
-    chooser.addOption("DriveTri", () -> new DriveTri(drive));
-
-    chooser.addOption("DriveDistance",
-        () -> drive.DriveDistanceCommand(angle.getDouble(0), distance.getDouble(0)));
-
-    chooser.addOption("ResetElevatorEncoder", () -> elevator.ResetElevatorEncoderCommand());
     chooser.addOption("RaiseElevator", () -> elevator.RaiseElevatorCommand(distance.getDouble(0)));
 
     chooser.addOption("LowerElevator", () -> elevator.LowerElevatorCommand(distance.getDouble(0)));
